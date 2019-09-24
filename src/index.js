50a067a0ea837af932b1c719af32c852aeba39d3
oleg kin <noklige@gmail.com>
2019-09-24 14:24:22 +0300

    feat: function implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let pairArray = []; //pairs to replace
+    for (let i = 0; i < bracketsConfig.length; i++)
+        pairArray.push(bracketsConfig[i][0] + bracketsConfig[i][1])
+    while (str.length>0){
+        let lenBefore=str.length;
+        for (let i = 0; i < pairArray.length; i++)
+            str=str.replace(new RegExp(pairArray[i].replace(/[.*+?^${}()|[\]\\]/g, '\\$&'),"g"), "");
+        if (lenBefore===str.length)
+            break;
+    }
+    return str.length===0;
 }
